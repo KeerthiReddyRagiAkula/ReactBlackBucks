600fffa7cadefa50ae5ece62f0bd033dc9af1152
Keerthi <139979610+KeerthiReddyRagiAkula@users.noreply.github.com>
2023-10-19 16:54:42 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+
+function App() {
+  const first="abbas";
+  const age="45";
+  const last="M";
+
+  return (
+    <div className="App">
+      <h1>Hi!!</h1>
+      <div>
+        I am {first}.{last}. My age is {age};
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
